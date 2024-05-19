ae3564c5060c33a2059cf900c2d25cb3eeed7d30
Metameg <ametzger08@gmail.com>
2024-05-18 18:07:05 -0600

    began copying kreative-medias services page

--- pages/services.php
+++ pages/services.php
@@ -4,27 +4,31 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Your Website</title>
-  <link rel="stylesheet" href="css/Services.css"> <!-- Link to your compiled CSS file -->
-  <link rel="stylesheet" href="css/navbar.css">
+  <link rel="stylesheet" href="../css/services.css"> <!-- Link to your compiled CSS file -->
+  <link rel="stylesheet" href="../css/navbar.css">
 
 </head>
 
 <body>
 
   <header>
-    <?php include 'config.php'; ?>
+    <?php include '../config.php'; ?>
     <script src="<?php echo $currentConfig['baseUrl'] . 'scripts/navbarController.js'?>"></script>
   </header>
 
-<!-- Hero Section -->
-<section class="hero-section">
-    <!-- <video autoplay muted loop class="hero-video">
-        <source src="assets/hero-section.mp4" type="video/mp4">
-        Your browser does not support the video tag.
-    </video> -->
-    <div class="hero-content">
-        <h1>Write Something Here</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        <button class="cta-button">Free Consultation</button>
-    </div>
-</section>
+  <!-- Hero Section -->
+  <section class="hero-section">
+      <!-- <video autoplay muted loop class="hero-video">
+          <source src="assets/hero-section.mp4" type="video/mp4">
+          Your browser does not support the video tag.
+      </video> -->
+      <div class="hero-content">
+        <div class="cta">
+          <h1>Web Development Services</h1>
+          <h2>Customized for Your Needs</h2>
+          <button class="cta-button">Contact Us</button>
+        </div>
+      </div>
+  </section>
+</body>
+</html>
