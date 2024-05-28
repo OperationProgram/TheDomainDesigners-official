7b762991dd6b2e47a07ee7996531aaf953319e98
Metameg <ametzger08@gmail.com>
2024-05-28 12:35:08 -0600

    finished page through contents section

--- src/pages/projects/dipgaldips.php
+++ src/pages/projects/dipgaldips.php
@@ -14,15 +14,29 @@
         <script src=" <?php echo $basePath . '/scripts/navbarController.js';?>"></script> 
     </header>
 
-    <div class="overlay"></div>
+    <!-- <div class="overlay"></div> -->
     <section class="hero-section">
-        <div class="hero-content">
-            <div class="cta">
-            <h1>Web Development Services</h1>
-            <h2>Customized for Your Needs</h2>
-            <button class="cta-button">Contact Us</button>
+    </section>
+
+    <section class="contents-section">
+        <div class="contents-container">
+            <div class="nav-column">
+                <div class="img-box">
+                    <img src="../../assets/dipgaldips/logo.jpg">
+                </div>
+                <ul>
+                    <a href="#"><li class="content-link">Wordpress Solution</li></a>
+                    <a href="#"><li class="content-link">Search Engine Optimization</li></a>
+                    <a href="#"><li class="content-link">Branding</li></a>
+                </ul>   
+            </div>
+            <div class="text-column">
+                <h2 class="custom-h2">Dip-Gal-Dips</h2>
+                <p>The GOT Brand aimed to increase their engagement, optimize their online presence, create high quality content, and enhance their visibility. The GOT Brand partnered with Kreative Media to harness our expertise in search engine optimization, social media management, website design, and content creation with high quality photography and videography. We developed a complete strategy tailored to elevate their brand, focusing on SEO to improve their search rankings and attract a more relevant audience. Our social media initiatives were designed to build a stronger community and increase interaction with the brand. By redesigning their website, we ensured a more engaging, intuitive, and conversion-optimized user experience. The integration of professional visual content distinctly showcased their offerings, distinguishing The GOT Brand in a competitive market. This integrated approach to digital marketing delivered a cohesive and elevated online presence, increasing their brand's visibility and engagement.
+                </p>
             </div>
         </div>
+
     </section>
 </body>
 
