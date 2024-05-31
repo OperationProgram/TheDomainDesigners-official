baf1b6cbe4be56db42bacad24e5bd58b683e5413
Metameg <ametzger08@gmail.com>
2024-05-31 11:45:55 -0600

    finished image replacement

--- src/pages/services/web-apps.php
+++ src/pages/services/web-apps.php
@@ -74,8 +74,8 @@
   <section id="section-1-header" class="section-5">
     <div class="sub-cta">
       <div class="cta-column">
-        <h2 class="custom-h2">Learn How Your Visitors Interact</h2>
-        <h1 class="custom-h1">Analytics</h1>
+        <h2 class="custom-h2">Sophisticated UI/UX</h2>
+        <h1 class="custom-h1">Frontend Technologies</h1>
         <p> Social media is no longer optional for businesses. It's a powerful platform to connect with customers, build brand loyalty, and drive sales. But managing multiple platforms, creating engaging content with high quality visuals, and analyzing results can be overwhelming.
          <br /><br />   
         That's where Kreative Media, a leading social media marketing company, comes in. Our visuals stand out so your brand can thrive, ensuring your messages resonate and capture your audience. We help businesses of all sizes develop and execute winning social media strategies that deliver real results. 
@@ -88,13 +88,18 @@
 
     <div class="icons">
       <div class="icon" data-aos="fade-down" data-aos-duration="1500">
-        <img src="../../assets/icons/google-analytics.svg">
-        <h3>Google Analytics</h3>
+        <img src="../../assets/icons/react.svg">
+        <h3>React</h3>
         <p>We don't just guess - we leverage data and analytics to inform our strategies and ensure optimal results for your brand. We track key metrics and constantly refine your campaigns for maximum impact.</p>
       </div>
       <div class="icon" data-aos="fade-up" data-aos-duration="1500">
-        <img src="../../assets/icons/link.svg">
-        <h3>Ahrefs</h3>
+        <img src="../../assets/icons/js.svg">
+        <h3>Es6 JS</h3>
+        <p>We don't just guess - we leverage data and analytics to inform our strategies and ensure optimal results for your brand. We track key metrics and constantly refine your campaigns for maximum impact.</p>
+      </div>
+      <div class="icon" data-aos="fade-up" data-aos-duration="1500">
+        <img src="../../assets/icons/bootstrap.svg">
+        <h3>Bootstrap</h3>
         <p>We don't just guess - we leverage data and analytics to inform our strategies and ensure optimal results for your brand. We track key metrics and constantly refine your campaigns for maximum impact.</p>
       </div>
     </div>
@@ -103,8 +108,8 @@
   <section id="section-2-header" class="section-5">
     <div class="sub-cta">
       <div class="cta-column">
-        <h2 class="custom-h2">Learn How Your Visitors Interact</h2>
-        <h1 class="custom-h1">Analytics</h1>
+        <h2 class="custom-h2">Building Your Architecture</h2>
+        <h1 class="custom-h1">Backend Technologies</h1>
         <p> Social media is no longer optional for businesses. It's a powerful platform to connect with customers, build brand loyalty, and drive sales. But managing multiple platforms, creating engaging content with high quality visuals, and analyzing results can be overwhelming.
          <br /><br />   
         That's where Kreative Media, a leading social media marketing company, comes in. Our visuals stand out so your brand can thrive, ensuring your messages resonate and capture your audience. We help businesses of all sizes develop and execute winning social media strategies that deliver real results. 
@@ -117,13 +122,18 @@
 
     <div class="icons">
       <div class="icon" data-aos="fade-down" data-aos-duration="1500">
-        <img src="../../assets/icons/google-analytics.svg">
-        <h3>Google Analytics</h3>
+        <img src="../../assets/icons/flask.svg">
+        <h3>Flask</h3>
         <p>We don't just guess - we leverage data and analytics to inform our strategies and ensure optimal results for your brand. We track key metrics and constantly refine your campaigns for maximum impact.</p>
       </div>
       <div class="icon" data-aos="fade-up" data-aos-duration="1500">
-        <img src="../../assets/icons/link.svg">
-        <h3>Ahrefs</h3>
+        <img src="../../assets/icons/nodejs.svg">
+        <h3>Node JS</h3>
+        <p>We don't just guess - we leverage data and analytics to inform our strategies and ensure optimal results for your brand. We track key metrics and constantly refine your campaigns for maximum impact.</p>
+      </div>
+      <div class="icon" data-aos="fade-up" data-aos-duration="1500">
+        <img src="../../assets/icons/aws.svg">
+        <h3>Amazon Web Services</h3>
         <p>We don't just guess - we leverage data and analytics to inform our strategies and ensure optimal results for your brand. We track key metrics and constantly refine your campaigns for maximum impact.</p>
       </div>
     </div>
@@ -132,8 +142,8 @@
   <section id="section-3-header" class="section-5">
     <div class="sub-cta">
       <div class="cta-column">
-        <h2 class="custom-h2">Learn How Your Visitors Interact</h2>
-        <h1 class="custom-h1">Analytics</h1>
+        <h2 class="custom-h2">Storing Data Securely</h2>
+        <h1 class="custom-h1">Database Management</h1>
         <p> Social media is no longer optional for businesses. It's a powerful platform to connect with customers, build brand loyalty, and drive sales. But managing multiple platforms, creating engaging content with high quality visuals, and analyzing results can be overwhelming.
          <br /><br />   
         That's where Kreative Media, a leading social media marketing company, comes in. Our visuals stand out so your brand can thrive, ensuring your messages resonate and capture your audience. We help businesses of all sizes develop and execute winning social media strategies that deliver real results. 
@@ -146,13 +156,8 @@
 
     <div class="icons">
       <div class="icon" data-aos="fade-down" data-aos-duration="1500">
-        <img src="../../assets/icons/google-analytics.svg">
-        <h3>Google Analytics</h3>
-        <p>We don't just guess - we leverage data and analytics to inform our strategies and ensure optimal results for your brand. We track key metrics and constantly refine your campaigns for maximum impact.</p>
-      </div>
-      <div class="icon" data-aos="fade-up" data-aos-duration="1500">
-        <img src="../../assets/icons/link.svg">
-        <h3>Ahrefs</h3>
+        <img src="../../assets/icons/sql.svg">
+        <h3>SQL</h3>
         <p>We don't just guess - we leverage data and analytics to inform our strategies and ensure optimal results for your brand. We track key metrics and constantly refine your campaigns for maximum impact.</p>
       </div>
     </div>
