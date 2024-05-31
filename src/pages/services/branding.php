035f692bb9c8b436ba0a73204a9165bffda2d7f6
Metameg <ametzger08@gmail.com>
2024-05-31 13:36:31 -0600

    completed image replacement

---
+++ src/pages/services/branding.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Your Website</title>
+  <link rel="stylesheet" href="../../css/service-partial.css">
+  <link rel="stylesheet" href="../../css/branding.css">
+  <link rel="stylesheet" href="../../css/navbar.css">
+  <link rel="stylesheet" href="../../css/footer.css">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <header>
+    <?php $basePath = '../..';?>
+    <?php include $basePath . '/pages/Navigation/navbar.php';?>
+    <script src=" <?php echo $basePath . '/scripts/navbarController.js';?>"></script>
+  </header>
+
+  <div class="overlay"></div>
+
+  <section class="hero-section">
+    <div class="hero-content">
+    <img class="background-hero-img" src="../../assets/purple-splashdrop.jpg">
+        <div class="cta">
+            <h2 class="custom-h2 hero-gradient">Stellar Visual Designs</h2>
+            <h1 class="custom-h1">Branding</h1>
+            <button class="cta-button">Contact Us</button>
+        </div>
+        <img class="hero-img" src="../../assets/icons/branding.svg">
+    </div>
+    <div class="hero-card-slide-container" data-aos="fade-right" data-aos-duration="1200">
+      <div class="hero-card">
+        <img src="../../assets/cosmetics.jpg">
+      </div>
+      <div class="hero-card">
+        <img src="../../assets/bar-signs.jpg">
+      </div>
+      <div class="hero-card">
+        <img src="../../assets/candle-brand.jpg">
+      </div>
+      <div class="hero-card">
+        <p>Help your brand grow seamlessly through social media platforms</p>
+      </div>
+    </div>
+  </section>
+  
+  <section class="wp-contents contents">
+    <div class="contents-container">
+      <a href="#section-1-header">      
+        <div class="content-title">
+          <h2>What We Offer</h2>
+          <img src="../../assets/right-arrow.svg" width="40" height="40">
+        </div>
+      </a>
+    </div>
+  </section>
+
+
+  <section id="section-1-header" class="section-3">
+    <img src="../../assets/blue-orange.jpg" class="show-sm-screen">
+    <div id="ps-left-column" class="text-column">
+      <h2 class="custom-h2">Build A Memory</h2>
+      <h1 class="custom-h1">What We Offer</h1>
+      <ul>
+        <li><p> <strong class="custom-bold">Social media is no longer optional:</strong> for businesses. 
+          It's a powerful platform to connect with customers, build brand
+          loyalty, and drive sales. But managing multiple platforms, 
+          creating engaging content with high quality visuals, and
+          analyzing results can be overwhelming.
+          <br /><br />   
+          That's where Kreative Media, a leading social media marketing company, comes in. Our visuals stand out so your brand can thrive, ensuring your messages resonate and capture your audience. We help businesses of all sizes develop and execute winning social media strategies that deliver real results. 
+        </p></li>
+        <br />
+        <li><p> <strong class="custom-bold">Social media is no longer optional:</strong> for businesses. 
+          It's a powerful platform to connect with customers, build brand
+          loyalty, and drive sales. But managing multiple platforms, 
+          creating engaging content with high quality visuals, and
+          analyzing results can be overwhelming.
+          <br /><br />   
+          That's where Kreative Media, a leading social media marketing company, comes in. Our visuals stand out so your brand can thrive, ensuring your messages resonate and capture your audience. We help businesses of all sizes develop and execute winning social media strategies that deliver real results. 
+        </p></li> 
+        <br />
+        <li><p> <strong class="custom-bold">Social media is no longer optional:</strong> for businesses. 
+          It's a powerful platform to connect with customers, build brand
+          loyalty, and drive sales. But managing multiple platforms, 
+          creating engaging content with high quality visuals, and
+          analyzing results can be overwhelming.
+          <br /><br />   
+          That's where Kreative Media, a leading social media marketing company, comes in. Our visuals stand out so your brand can thrive, ensuring your messages resonate and capture your audience. We help businesses of all sizes develop and execute winning social media strategies that deliver real results. 
+        </p></li>
+        <br />
+        <li><p> <strong class="custom-bold">Social media is no longer optional:</strong> for businesses. 
+          It's a powerful platform to connect with customers, build brand
+          loyalty, and drive sales. But managing multiple platforms, 
+          creating engaging content with high quality visuals, and
+          analyzing results can be overwhelming.
+          <br /><br />   
+          That's where Kreative Media, a leading social media marketing company, comes in. Our visuals stand out so your brand can thrive, ensuring your messages resonate and capture your audience. We help businesses of all sizes develop and execute winning social media strategies that deliver real results. 
+        </p></li>
+      </ul>
+      <button class="cta-button-dark">Let's Chat</button>
+    </div>
+    
+    <div class="img-container">
+      <div class="img-box">
+        <img src="../../assets/blue-orange.jpg">
+      </div>
+      <div class="img-box">
+        <img src="../../assets/brands-orange-book.jpg">
+      </div>
+      <div class="img-box">
+        <img src="../../assets/computer-color-schemes.jpg">
+      </div>
+      <div class="img-box">
+        <img src="../../assets/brands-armful.jpg">
+      </div>
+      <div class="img-box">
+        <img src="../../assets/color-schemes.jpg">
+      </div>
+    </div>
+
+    <div class="clearfix"></div>
+
+  </section>
+
+  <script src="../../scripts/partialScrollContainer.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+  <script>
+    AOS.init({
+      once: true
+    });
+  </script>
+
+  <?php include '../Footer/footer.php';?>
+</body>
