d15cd8ad5dbb4fcfb0ac3df3b9f5d354f08463c3
Metameg <ametzger08@gmail.com>
2024-06-05 10:15:31 -0600

    more alt tags

--- src/pages/services.php
+++ src/pages/services.php
@@ -26,6 +26,7 @@
   <!-- Hero Section -->
   <div class="overlay"></div>
   <section class="hero-section">
+    <img class="sr-only" alt="Open Laptop on a Desk" src="../assets/custom-code-services.jpg" />
       <div class="hero-content">
         <!-- <div class="cta"> -->
           <h1>Web Development Services</h1>
@@ -38,13 +39,13 @@
   <section id="services">
     <div class="service-row">
       <div class="service-card large-card">
-          <img src="../assets/blog-letters.jpg" >
+          <img src="../assets/blog-letters.jpg" alt="Building blogs with WordPress">
           <h2>Wordpress Sites</h2>
           <a href="services/wordpress-sites.php"><button class="cta-button">Learn More</button></a>
       </div>
 
       <div class="service-card small-card">
-          <img src="../assets/google.jpg" >
+          <img src="../assets/google.jpg" alt="Boost Your Website's Visibility: Powerful SEO for Startups">
           <h2>SEO</h2>
           <a href="services/seo.php"><button class="cta-button">Learn More</button></a>
       </div>
@@ -52,13 +53,13 @@
 
     <div class="service-row">
       <div class="service-card small-card">
-          <img src="../assets/phone-plant-dark.jpg" >
+          <img src="../assets/phone-plant-dark.jpg" alt="Web apps that help your business flourish">
           <h2>Web Apps</h2>
           <a href="services/web-apps.php"><button class="cta-button">Learn More</button></a>
       </div>
 
       <div class="service-card small-card">
-          <img src="../assets/computer-glasses.jpg" >
+          <img src="../assets/computer-glasses.jpg" alt="Maintaining website performance with code expertise">
           <h2>Maintenance</h2>
           <a href="services/maintenance.php"><button class="cta-button">Learn More</button></a>
       </div>
@@ -66,20 +67,20 @@
 
     <div class="service-row">
       <div class="service-card large-card">
-          <img src="../assets/social-media-services.jpg" >
+          <img src="../assets/social-media-services.jpg" alt="Managing your social media presence across platforms">
           <h2>Social Media Marketing</h2>
           <a href="services/social-media.php"><button class="cta-button">Learn More</button></a>
       </div>
 
       <div class="service-card small-card">
-          <img src="../assets/branding.jpg" >
+          <img src="../assets/branding.jpg" alt="We create memorable logos and brand elements">
           <h2>Branding</h2>
           <a href="services/branding.php"><button class="cta-button">Learn More</button></a>
       </div>
     </div>
     
     <div class="lets-chat">
-        <img src="../assets/laptop-side.png" data-aos="fade-right">
+        <img src="../assets/laptop-side.png" data-aos="fade-right" alt="Open laptop computer">
         <h2>Let's Chat</h2>
         <button class="cta-button">Contact Us</button>
     </div>
