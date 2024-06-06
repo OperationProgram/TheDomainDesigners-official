5355a681637b3d31ecfff876c5956276ff6abcb9
Metameg <ametzger08@gmail.com>
2024-06-06 10:20:34 -0600

    content written

--- src/pages/about.php
+++ src/pages/about.php
@@ -36,13 +36,29 @@
       <div class="about-text">
         <h2>About Us</h2>
         <h3>Our Mission</h3>
-        <p>Our mission is to provide the best services to our clients with utmost dedication and professionalism.</p>
-        <p>We strive to deliver exceptional results and continuously improve our offerings to meet the evolving needs of our customers.</p>
-        <a href="contact.php"><button>Learn More</button></a>
+        <p>At TheDomainDesigners, our mission is to empower startups with innovative and user-friendly web app and website solutions. We understand the unique challenges faced by startups in today's competitive landscape. That's why we focus on crafting digital experiences that not only look great but also drive growth and engagement.<br/><br/>
+
+        Here's how we achieve our mission:</p>
+        <ul>
+          <li><p><strong class="custom-bold">Collaborative Strategy: </strong>We believe in a collaborative approach. We work closely with you, our startup client, to understand your specific goals, target audience, and budget. Through open communication, we determine the perfect blend of services and technologies to bring your vision to life.</p>
+
+          <li><p><strong class="custom-bold">Building for the Future: </strong>We stay ahead of the curve with cutting-edge technologies and design trends, ensuring your startup website is future-proof.</p>
+
+          <li><p><strong class="custom-bold">User-Centric Focus: </strong>We prioritize user experience (UX) in everything we do, creating intuitive and engaging websites that convert visitors into customers.</p>
+
+          <li><p><strong class="custom-bold">Scalability and Growth: </strong> We design websites and web apps that can scale alongside your thriving startup, adapting to your evolving needs.</p>
+
+          <li><p><strong class="custom-bold">Startup-Friendly Approach:</strong>We understand your budget and timelines. We offer flexible solutions and transparent communication to ensure a smooth and successful partnership.</p>
+        </ul>
+
+        <p>By partnering with TheDomainDesigners, you gain a dedicated team passionate about helping startups succeed in the digital world. We don't just build websites; we become an extension of your team, working tirelessly to translate your vision into a powerful online presence.
+        </p>
+
+        <a href="contact.php"><button>Let's Chat</button></a>
 
       </div>
       <div class="about-image">
-        <img src="../assets/skull-min.jpg" alt="About Us Image">
+        <img src="../assets/skull-min.jpg" alt="About Us Decorative Skull ">
       </div>
     </div>
   </section>
@@ -51,7 +67,12 @@
   <section class="our-story-section">
     <div class="container">
       <h2>Our Story</h2>
-      <p>Share the history of your company, how it started, and its journey so far. Use a timeline or narrative format with key milestones, challenges, and achievements.</p>
+      <p>TheDomainDesigners isn't your typical web design agency.
+ We're a Houston, TX-born startup founded in 2024 by two passionate developers, Alex Metzger and Spencer Holan. Our story began way back in high school, where our shared love for coding ignited a spark that would eventually blossom into this collaborative venture. <br/><br/>
+ <strong class="custom-bold">Alex Metzger</strong>, a graduate of Southern New Hampshire University (SNHU) with a Bachelor of Science in Computer Science, is currently pursuing a Master's degree in Artificial Intelligence at the University of North Texas. His thirst for knowledge and dedication to pushing boundaries fuels our innovative approach. <br/><br/>
+ <strong class="custom-bold">Spencer Holan</strong>, a current computer science student, brings a wealth of real-world experience to the table. Since 2022, he's been honing his skills as a freelance web developer, tackling diverse projects and gaining valuable insights into client needs. <br/><br/>
+ We believe that the future is built on collaboration and shared passion. That's why we prioritize open communication and a genuine understanding of your startup's vision. We're not just a web design company; we're your tech partners, eager to translate your ideas into a powerful digital reality.
+</p>
     </div>
   </section>
 
@@ -61,27 +82,27 @@
       <div class="values-grid">
         <div class="value-item">
           <h3>Integrity</h3>
-          <p>We uphold the highest standards of integrity in all our actions.</p>
+          <p>Honesty, transparency, and ethical conduct are paramount in our work. We build trust by being upfront and reliable, ensuring our actions align with our promises.</p>
         </div>
         <div class="value-item">
-          <h3>Customer Focus</h3>
-          <p>We value our customers and strive to meet their needs and exceed their expectations.</p>
+          <h3>Client Success</h3>
+          <p>Your goals are our north star. We listen closely, then craft solutions that drive results and fuel your startup's growth.</p>
         </div>
         <div class="value-item">
           <h3>Collaboration</h3>
-          <p>Fostering teamwork, communication, and inclusivity to achieve common goals.</p>
+          <p>We combine our expertise with yours for powerful results. Open communication and collaboration are key to achieving your startup's vision.</p>
         </div>
         <div class="value-item">
           <h3>Innovation</h3>
-          <p>Embracing creativity, curiosity, and forward-thinking to drive progress.</p>
+          <p>We're obsessed with progress. We constantly explore new technologies to craft cutting-edge solutions that keep your startup ahead of the curve.</p>
         </div>
         <div class="value-item">
           <h3>Excellence</h3>
-          <p>Committing to environmental stewardship, social responsibility, and long-term viability.</p>
+          <p>We set the bar high. We meticulously craft exceptional web app and website solutions that deliver superior quality and push the boundaries of what's possible.</p>
         </div>
         <div class="value-item">
           <h3>Sustainability</h3>
-          <p>We value our customers and strive to meet their needs and exceed their expectations.</p>
+          <p>While web development resources can't be planted, we strive for long-lasting, eco-conscious solutions for our clients.</p>
         </div>
       </div>
     </div>
@@ -91,13 +112,13 @@
     <h2>Meet the Team</h2>
     <div class="team-grid">
       <div class="team-card">
-        <img src="../assets/alex-pic.png" alt="Team Member Name">
+        <img src="../assets/alex-pic.png" alt="TheDomainDesigners CoFounder - Alex Metzger">
         <h3>Alex Metzger</h3>
         <p>Co-Founder</p>
         <!-- <p>Brief description or bio of the team member.</p> -->
       </div>
       <div class="team-card">
-        <img src="../assets/spencer-pic.png" alt="Team Member Name">
+        <img src="../assets/spencer-pic.png" alt="TheDomainDesigners CoFounder - Spencer Holan">
         <h3>Spencer Holan</h3>
         <p>Co-Founder</p>
         <!-- <p>Brief description or bio of the team member.</p> -->
