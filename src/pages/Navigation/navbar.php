e0fd9ce156b170e4cfd78ee9522d508b5eae6357
OperationProgram <programmingmylif3@gmail.com>
2024-06-17 21:53:34 -0500

    pushing to CR

--- src/pages/Navigation/navbar.php
+++ src/pages/Navigation/navbar.php
@@ -1,21 +1,25 @@
-<div class="navbar-wrapper">
-<div class="container">
 
 <nav class="navbar">
-    <div class="logo-container">
-        <a class="logo-link" href="<?php echo $basePath; ?>/"><img class="logo" src="<?php echo $basePath; ?>/assets/good/custom-logo.png" alt="TheDomainDesigners Logo"></a>
-    </div>
+    <!-- <div class="logo-container">
+        <a class="logo-link" href="<?php echo $basePath; ?>/"><img class="logo" src="<?php echo $basePath; ?>/assets/tdd_logo.svg" alt="TheDomainDesigners Logo"></a>
+    </div> -->
     <ul class="nav-menu" id="nav-menu">
-        <a class="logo-link logo-hidden" href="<?php echo $basePath; ?>/"><img class="logo" src="<?php echo $basePath; ?>/assets/custom-logo.png" alt="TheDomainDesigners Logo"></a>
-        <li><a href="<?php echo $basePath; ?>/">Home</a></li>
-        <li><a href="<?php echo $basePath; ?>/newhome.php">Home Beta</a></li>
+        <a class="logo-link logo-hidden" href="<?php echo $basePath; ?>/"><img class="logo" src="<?php echo $basePath; ?>/assets/tdd_logo.svg" alt="TheDomainDesigners Logo"></a>
+        <!-- <li><a href="<?php echo $basePath; ?>/">Home</a></li> -->
+        <!-- <li><a href="<?php echo $basePath; ?>/newhome.php">Home Beta</a></li> -->
+
         <li><a href="<?php echo $basePath; ?>/pages/services.php">Services</a></li>
         <li><a href="<?php echo $basePath; ?>/pages/our-work.php">Our Work</a></li>
-
+        <div class="logo-container">
+            <a class="logo-link" href="<?php echo $basePath; ?>/"><img class="logo" src="<?php echo $basePath; ?>/assets/tdd_logo.svg" alt="TheDomainDesigners Logo"></a>
+        </div>
         <li><a href="<?php echo $basePath; ?>/pages/about.php">Our Team</a></li>
         <li><a href="<?php echo $basePath; ?>/pages/contact.php">Contact Us</a></li>
-
+        <div class="social-links-container-hamburger">
+            <a href="https://www.instagram.com/thedomaindesigners/"><img src="<?php echo $basePath; ?>/assets/icons/instagram.svg" width="20" height="20"></a>
+        </div>
     </ul>
+
     <div class="social-links-container">
         <a href="https://www.instagram.com/thedomaindesigners/"><img src="<?php echo $basePath; ?>/assets/icons/instagram.svg" width="20" height="20"></a>
     </div>
@@ -23,6 +27,3 @@
         <span class="hamburger-icon" id="hamburger-icon">&#9776;</span>
     </div>
 </nav>
-</div>
-</div>
-
