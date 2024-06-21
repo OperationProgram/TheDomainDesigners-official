46c823cf0057d4fdeee31a1372bcba9af927e556
Metameg <ametzger08@gmail.com>
2024-06-21 14:02:30 -0600

    mobile overflow fix attempt

--- src/index.php
+++ src/index.php
@@ -16,6 +16,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
 </head>
+<div class="hide-overlay">
 <body>
     <header>  
         <?php $basePath = '.';?>
@@ -315,4 +316,5 @@
 
 
 </body>
+</div>
 </html>
