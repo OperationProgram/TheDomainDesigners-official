fb3a3ab1e5ae2eeb2fb8a79417c149bb74cab08d
Metameg <ametzger08@gmail.com>
2024-06-25 10:29:37 -0600

    newservices page created

---
+++ src/pages/newservices.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Domain Designers | Hire Web Designers & Developers - Launch Your Vision </title>
+    <meta name="description" content="Hire web designers & developers at The Domain Designers. We craft user-friendly websites that empower startups to thrive. Get a free consultation today!">
+    <meta name="robots" content="noindex,nofollow"> <!-- Remove in production -->
+    <meta name="keywords" content="web design, web developers, hire web designers, startup web design, development for startups, Houston web design, The Domain Designers">
+    <meta name="author" content="The Domain Designers">
+    <link rel="icon" type="image/x-icon" href="assets/tdd_logo.png">
+    <link rel="stylesheet" href="css/navbar.css"> <!-- Link to your homepage CSS -->
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+</head>
+        
+<body>
+<div class="hide-overlay">
+    <header>  
+        <?php $basePath = '..';?>
+        <?php include $basePath . '/pages/Navigation/navbar.php';?>
+        <script src=" <?php echo $basePath . '/scripts/navbarController.js';?>"></script>
+    </header>
+    <section class="hero">
+        <div class="overlay-1"></div>
+        <div class="overlay-2"></div>
+        <div class="container hero-container">
+            <div class="hero-content" data-aos="slide-right" data-aos-once="true">
+                <h1>Design Your Dream with Professionals</h1>
+                <p >Transform your online presence with our hand-coded websites. Starting at $500, we offer customized designs, template solutions, eCommerce setups, and more.</p>
+                <a href="#" class="cta-button"  data-aos="zoom-in" data-aos-once="true">CONTACT US</a>
+            </div>
+            <div class="hero-image">
+                <img src="/src/assets/good/dev.svg" alt="Hero Image" data-aos="slide-right" data-aos-once="true">
+            </div>
+        </div>
+    </section>
+
+    <div class="border-divider"></div>
+</div>
+</body>
+</html>
