037caeeb4815ae9ab08f37313146974b261987fe
Metameg <ametzger08@gmail.com>
2024-07-25 14:58:23 -0600

    event listeners changed to allow mobile scrolling

--- src/scripts/mouseEffect.js
+++ src/scripts/mouseEffect.js
@@ -677,39 +677,38 @@ function resizeCanvas() {
   }
 }
 
-canvas.addEventListener('mousemove', e => {
+let isScrolling = false;
+let startTouchY = 0;
 
-  pointers[0].moved = pointers[0].down;
+// Mouse events
+canvas.addEventListener('mousemove', e => {
+  if (!pointers[0].down) return;
+  pointers[0].moved = true;
   pointers[0].dx = (e.offsetX - pointers[0].x) * 10.0;
   pointers[0].dy = (e.offsetY - pointers[0].y) * 10.0;
   pointers[0].x = e.offsetX;
   pointers[0].y = e.offsetY;
 });
 
-canvas.addEventListener('touchmove', e => {
-  e.preventDefault();
-  const touches = e.targetTouches;
-  for (let i = 0; i < touches.length; i++) {
-    let pointer = pointers[i];
-    pointer.moved = pointer.down;
-    pointer.dx = (touches[i].pageX - pointer.x) * 10.0;
-    pointer.dy = (touches[i].pageY - pointer.y) * 10.0;
-    pointer.x = touches[i].pageX;
-    pointer.y = touches[i].pageY;
-  }
-}, false);
-
-canvas.addEventListener('mousemove', () => {
+canvas.addEventListener('mousedown', e => {
   pointers[0].down = true;
   pointers[0].color = [Math.random() + 0.2, Math.random() + 0.2, Math.random() + 0.2];
+  pointers[0].x = e.offsetX;
+  pointers[0].y = e.offsetY;
 });
 
+canvas.addEventListener('mouseup', () => {
+  pointers[0].down = false;
+});
+
+// Touch events
 canvas.addEventListener('touchstart', e => {
-  e.preventDefault();
+  startTouchY = e.touches[0].clientY;
+  isScrolling = false;
   const touches = e.targetTouches;
   for (let i = 0; i < touches.length; i++) {
     if (i >= pointers.length)
-    pointers.push(new pointerPrototype());
+      pointers.push(new pointerPrototype());
 
     pointers[i].id = touches[i].identifier;
     pointers[i].down = true;
@@ -717,16 +716,95 @@ canvas.addEventListener('touchstart', e => {
     pointers[i].y = touches[i].pageY;
     pointers[i].color = [Math.random() + 0.2, Math.random() + 0.2, Math.random() + 0.2];
   }
-});
+}, { passive: true });
 
+canvas.addEventListener('touchmove', e => {
+  const currentTouchY = e.touches[0].clientY;
+  if (Math.abs(currentTouchY - startTouchY) > 10) {
+    isScrolling = true;
+  }
+  if (!isScrolling) {
+    e.preventDefault();
+    const touches = e.targetTouches;
+    for (let i = 0; i < touches.length; i++) {
+      let pointer = pointers[i];
+      pointer.moved = true;
+      pointer.dx = (touches[i].pageX - pointer.x) * 10.0;
+      pointer.dy = (touches[i].pageY - pointer.y) * 10.0;
+      pointer.x = touches[i].pageX;
+      pointer.y = touches[i].pageY;
+    }
+  }
+}, { passive: false });
+
+canvas.addEventListener('touchend', e => {
+  const touches = e.changedTouches;
+  for (let i = 0; i < touches.length; i++) {
+    for (let j = 0; j < pointers.length; j++) {
+      if (touches[i].identifier == pointers[j].id) {
+        pointers[j].down = false;
+      }
+    }
+  }
+}, { passive: true });
+
+// Window events
 window.addEventListener('mouseleave', () => {
   pointers[0].down = false;
 });
 
-window.addEventListener('touchend', e => {
-  const touches = e.changedTouches;
-  for (let i = 0; i < touches.length; i++)
-    for (let j = 0; j < pointers.length; j++)
-      if (touches[i].identifier == pointers[j].id)
-        pointers[j].down = false;
-});
+
+
+// canvas.addEventListener('mousemove', e => {
+
+//   pointers[0].moved = pointers[0].down;
+//   pointers[0].dx = (e.offsetX - pointers[0].x) * 10.0;
+//   pointers[0].dy = (e.offsetY - pointers[0].y) * 10.0;
+//   pointers[0].x = e.offsetX;
+//   pointers[0].y = e.offsetY;
+// });
+
+// canvas.addEventListener('touchmove', e => {
+//   e.preventDefault();
+//   const touches = e.targetTouches;
+//   for (let i = 0; i < touches.length; i++) {
+//     let pointer = pointers[i];
+//     pointer.moved = pointer.down;
+//     pointer.dx = (touches[i].pageX - pointer.x) * 10.0;
+//     pointer.dy = (touches[i].pageY - pointer.y) * 10.0;
+//     pointer.x = touches[i].pageX;
+//     pointer.y = touches[i].pageY;
+//   }
+// }, false);
+
+// canvas.addEventListener('mousemove', () => {
+//   pointers[0].down = true;
+//   pointers[0].color = [Math.random() + 0.2, Math.random() + 0.2, Math.random() + 0.2];
+// });
+
+// canvas.addEventListener('touchstart', e => {
+//   e.preventDefault();
+//   const touches = e.targetTouches;
+//   for (let i = 0; i < touches.length; i++) {
+//     if (i >= pointers.length)
+//     pointers.push(new pointerPrototype());
+
+//     pointers[i].id = touches[i].identifier;
+//     pointers[i].down = true;
+//     pointers[i].x = touches[i].pageX;
+//     pointers[i].y = touches[i].pageY;
+//     pointers[i].color = [Math.random() + 0.2, Math.random() + 0.2, Math.random() + 0.2];
+//   }
+// });
+
+// window.addEventListener('mouseleave', () => {
+//   pointers[0].down = false;
+// });
+
+// window.addEventListener('touchend', e => {
+//   const touches = e.changedTouches;
+//   for (let i = 0; i < touches.length; i++)
+//     for (let j = 0; j < pointers.length; j++)
+//       if (touches[i].identifier == pointers[j].id)
+//         pointers[j].down = false;
+// });
